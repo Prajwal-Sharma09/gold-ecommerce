8b758138f496537cd4ed5436c011ce590a8ecb7f
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-07-18 15:13:26 +0545

    basic header and footer setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 import "./App.css";
 const App = () => {
-  return <div>Welcome to Gold Ecommerce..</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Gold Ecom..</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
