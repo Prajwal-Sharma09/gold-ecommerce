aaad90da32a2ec765f9f6503c09ec57602e74aa1
Prajwal-Sharma09 <prajwalsharmawqu09@gmail.com>
2021-07-24 00:19:53 +0545

    applied custom error handling

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -24,13 +24,13 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    //   const data = proudcts.find((p) => p._id === req.params.id);
     const product = await Product.findById(req.params.id);
 
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found." });
+      res.status(404);
+      throw new Error("Product not Found");
     }
   })
 );
